c78c450b45f8843fa306e17cc0ceb42aedb8fd14
Niilo <114094572+NLowoDev@users.noreply.github.com>
2022-10-15 19:01:30 +0300

    Add files via upload

---
+++ iteeeeeeeeeeeeeeeeeeeeeeeeeeeemit.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('metch_pooch', 'Metamfetamiini Pussi', 5, 0, 1),
+    ('meth', 'Metamfetamiini', 1, 0, 1),
+    ('coke_pooch', 'Kokaiini Pussi', 5, 0, 1),
+    ('coke', 'Kokaiini', 1, 0, 1),
